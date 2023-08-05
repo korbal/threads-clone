9f3e54040d85072f8b642782ec7e7ff5c2cabc43
Balint Korosi <balint.j.korosi@gmail.com>
2023-08-05 11:52:01 +0200

    post styling done

--- components/Post.vue
+++ components/Post.vue
@@ -25,9 +25,74 @@
               size="18"
             />
           </button>
+          <div
+            v-if="isMenu"
+            class="absolute border border-gray-600 right-0 z-20 mt-1 rounded"
+          >
+            <button
+              class="flex items-center rounded gap-2 text-red-500 justify-between bg-black w-full pl-4 pr-3 py-1 hover:bg-gray-900"
+            >
+              <div>Delete</div>
+              <Icon name="solar:trash-bin-trash-broken" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="relative flex items-center">
+        <div class="w-[42px] mx-auto">
+          <div class="absolute ml-4 mt-1 top-0 w-[1px] bg-gray-700 h-full" />
+        </div>
+        <div
+          class="bg-black rounded-lg w-[calc(100%-50px)] text-sm w-full font-light"
+        >
+          <div class="py-2 text-gray-300">{{ post.text }}</div>
+          <img
+            v-if="post && post.picture"
+            :src="post.picture"
+            alt=""
+            class="mx-auto w-full mt-2 pr-2 rounded"
+          />
+          <div class="absolute mt-2 w-full ml-2">
+            <button :disabled="isLike" class="flex items-center gap-1">
+              <Icon
+                class="p-1 text-white hover:bg-gray-800 rounded-full cursor-pointer"
+                name="mdi:cards-heart-outline"
+                size="28"
+              />
+            </button>
+            <div class="relative text-sm text-gray-500">
+              <div><span>4</span> likes</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
+    <div class="relative inline-block text-gray-500 pt-1 pb-1.5">
+      <div class="flex items-center">
+        <div class="flex items-center flex-wrap text-white gap-1 w-[42px]">
+          <div class="flex gap-0.5">
+            <img
+              src="https://picsum.photos/id/202/50"
+              class="rounded-full h-[14px] mt-2"
+              alt=""
+            />
+            <img
+              src="https://picsum.photos/id/223/50"
+              class="rounded-full h-[17px]"
+              alt=""
+            />
+          </div>
+          <div class="flex items-center">
+            <img
+              class="rounded-full h-[11px] ml-4"
+              src="https://picsum.photos/id/99/50"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="h-[1px] bg-gray-800 w-full mt-3" />
   </div>
 </template>
 
